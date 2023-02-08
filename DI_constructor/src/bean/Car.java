95100758978e379d2c68d3affedf4edc43432ea8
Umesh M <77375412+umeshmm@users.noreply.github.com>
2023-02-08 13:50:26 +0530

    Add files via upload

---
+++ DI_constructor/src/bean/Car.java
@@ -0,0 +1,13 @@
+package bean;
+
+public class Car {
+	private String carname;
+
+	public void getCarname() {
+		System.out.println("car name is "+ carname);
+	}
+
+	public void setCarname(String carname) {
+		this.carname = carname;
+	}
+}
